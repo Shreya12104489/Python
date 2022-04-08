f4996720dbaab2f009d05acc85af7272c9e76318
Shreya120399 <103144587+Shreya12104489@users.noreply.github.com>
2022-04-08 11:56:41 +0530

    add black jack python file

---
+++ Day 1/BlackJack.py
@@ -0,0 +1,142 @@
+############### Blackjack Project #####################
+import random
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+##################### Hints #####################
+
+#Hint 1: Go to this website and try out the Blackjack game: 
+#   https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here: 
+#   http://blackjack-final.appbrewery.repl.run
+
+#Hint 2: Read this breakdown of program requirements: 
+#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+#Then try to create your own flowchart for the program.
+
+#Hint 3: Download and read this flow chart I've created: 
+#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+  return random.choice(cards)
+
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+computer_cards = []
+for x in range(0,2):
+  a=deal_card()
+  user_cards.append(a)
+  b=deal_card()
+  computer_cards.append(b)
+  
+
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+def calculate_score(list):
+  
+  if len(list)==2:
+    if list[0]==11:
+      if list[1]==10:
+        return 0
+    elif list[0]==10:
+      if list[1]==11:
+        return 0
+  if 11 in list:
+    if sum(list)>21:
+      list.remove(11)
+      list.append(1)
+  return(sum(list))
+
+#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+
+#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+user_score=calculate_score(user_cards)
+computer_score=calculate_score(computer_cards)
+print(user_cards,computer_cards[0])
+
+game_ends=0
+if user_score==0 or user_score>21 or computer_score>21 or computer_score==0:
+  game_ends=1
+def check_score():
+  user_score=calculate_score(user_cards)
+  computer_score=calculate_score(computer_cards)
+  game_ends=0
+  if user_score==0 or user_score>21 or computer_score>21 or computer_score==0:
+    game_ends=1
+   
+  if game_ends==0:
+    print('Type "yes" if you want to draw another card or else type "no"')
+    ans=str(input())
+    if ans=="no":
+      game_ends==1
+      while(computer_score<17):
+        m=deal_card()
+        computer_cards.append(m)
+        computer_score=calculate_score(computer_cards)
+        
+      
+    elif ans=="yes":
+      user_cards.append(deal_card())
+      if sum(user_cards)>21:
+        print("You Lost!")
+        exit()
+      else:
+        print(user_cards)
+        check_score()
+check_score()  
+
+def compare(user_score,computer_score):
+  
+  print(user_cards,computer_cards)
+  user_score=calculate_score(user_cards)
+  computer_score=calculate_score(computer_cards)
+  if user_score == computer_score:
+    print("Draw")
+  elif user_score>21:
+    print("You Lost!")
+  elif computer_score>21:
+    print("You Won!")
+  elif len(computer_cards)==2 and computer_cards[0]==11 and computer_cards[1]==10:
+    print("You Lost! Computer has a black jack")
+  elif len(computer_cards)==2 and computer_cards[1]==11 and computer_cards[0]==10:
+    print("You Lost! Computer has a black jack")
+  elif len(user_cards)==2 and user_cards[0]==11 and user_cards[1]==10:
+    print("You Won! You have a BlackJack")
+  elif len(user_cards)==2 and user_cards[1]==11 and user_cards[0]==10:
+    print("You Won! You have a BlackJack")
+  else: 
+    
+    if user_score>computer_score:
+      print("You Won!")
+    if computer_score>user_score:
+      print("You Lost! ")
+compare(user_score,computer_score)
+#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+
+#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+
+#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+
+#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+
+#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
